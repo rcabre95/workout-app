69b42ba89b2ee1ba3524d19b68fc79d3e1c36d0b
rcabre95 <raphael.cabrera111@gmail.com>
2023-08-25 22:31:46 -0500

    added trainer and client onboarding forms as components, added login authentication to allow access to page

--- client/pages/onboard.tsx
+++ client/pages/onboard.tsx
@@ -3,8 +3,13 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import ClientOnboarding from "../components/auth-forms/ClientOnboarding";
 import TrainerOnboarding from "../components/auth-forms/TrainerOnboarding";
+import { logOut } from "../lib/helpers/auth";
 
 export type AccountType = "trainer" | "client" | null
+export interface TName {
+    first_name: string;
+    last_name: string;
+}
 
 // fix "any" types
 
@@ -12,8 +17,8 @@ export default function Onboard() {
     const router = useRouter();
     const [accountType, setAccountType] = useState<AccountType>();
     const [page, setPage] = useState<number>(1);
-    const [userId, setUserId] = useState<any>();
-    const [name, setName] = useState<any>(null)
+    const [userId, setUserId] = useState<string>("");
+    const [name, setName] = useState<TName | null>(null)
 
     const selectTrainer = () => {
         setAccountType("trainer");
@@ -25,26 +30,52 @@ export default function Onboard() {
         setPage(2);
     }
 
-    const logOut = async () => {
-        const { error } = await supabase.auth.signOut();
-
-        if (error) {
-            console.log("there was a problem logging you out.")
-        } else {
-            console.log("logged out successfully");
-            router.push("/")
-        }
+    const updateSupabaseAccType: () => Promise<void> = async () => {
+        const { data, error } = await supabase.auth.updateUser({
+            data: {
+                last_name: name?.last_name,
+                first_name: name?.first_name,
+                accnt_type: accountType
+            }
+        })
+        if (error) { console.log(error) };
+        if (data) { console.log(data) };
     }
 
     useEffect(() => {
         supabase.auth.getSession().then(d => {
-            if (d.error) {
-                console.log("user not logged in...")
-                router.push("/")
-            }
-            if (d.data) {
-                setUserId(d.data.session?.user.id)
-                setName(d.data.session?.user.user_metadata)
+            console.log(d)
+            if (d.data.session) {
+                console.log("session is active")
+                console.log(d.data.session.user.id)
+                setUserId(d.data.session.user.id);
+                setName(d.data.session.user.user_metadata as TName);
+                supabase.from('clients')
+                        .select()
+                        .eq('id', d.data.session.user.id)
+                        .then(d => {
+                            if (d.data) {
+                                if (d.data.length > 0) {
+                                    console.log("already exists in clients");
+                                    router.push("/dashboard")
+                                }
+                            }
+                        });
+
+                supabase.from('trainers')
+                        .select()
+                        .eq('id', d.data.session.user.id)
+                        .then(d => {
+                            if (d.data) {
+                                if (d.data.length > 0) {
+                                    console.log("already exists in trainers");
+                                    router.push("/dashboard")
+                                }
+                            }
+                        });
+                
+            } else {
+                console.log("there was an error...")
             }
         })
     }, [])
@@ -59,17 +90,18 @@ export default function Onboard() {
                 <button className="border-1 border-blue-300 bg-slate-400 p-3" type="button" onClick={selectClient}>
                     Client
                 </button>
+                {/* <span>{userId}</span> */}
             </div>
 
             {
             accountType === "trainer" ? 
-                <TrainerOnboarding />
+                <TrainerOnboarding accType={accountType} updateSupabaseAccType={updateSupabaseAccType} />
             : accountType === "client" ?
-                <ClientOnboarding />
+                <ClientOnboarding accType={accountType} userId={userId} updateSupabaseAccType={updateSupabaseAccType} />
             : null
             }
 
-            <button onClick={logOut} type="button">Log out</button>
+            <button onClick={() => logOut(router)} type="button">Log out</button>
         </div>
     )
 }
